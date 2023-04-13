da7a4dcf74fd4794b5e6f8bc33bd883f475096fa
Luana Hamaishi <lhamaishi@korewireless.com>
2023-04-12 21:35:14 -0300

    Adicionando a segunda tela de cadastros e a tela de redefinir senha

---
+++ bodyart-android-app/BodyArtMobile/app/src/main/java/com/sptech/bodyartmobile/Cadastro2.kt
@@ -0,0 +1,11 @@
+package com.sptech.bodyartmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Cadastro2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro2)
+    }
+}
