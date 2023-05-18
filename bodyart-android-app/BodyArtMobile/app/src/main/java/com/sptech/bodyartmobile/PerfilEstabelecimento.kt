7ab664ab5512b7eaa6dae5065cff4ca65243cb2e
Luana Hamaishi <lhamaishi@korewireless.com>
2023-05-18 16:08:44 -0300

    perfil estabelecimento v1

---
+++ bodyart-android-app/BodyArtMobile/app/src/main/java/com/sptech/bodyartmobile/PerfilEstabelecimento.kt
@@ -0,0 +1,11 @@
+package com.sptech.bodyartmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PerfilEstabelecimento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil_estabelecimento)
+    }
+}
