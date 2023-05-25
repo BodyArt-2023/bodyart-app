04720b78de432457ce5036a224a3a9c903b91d4a
Luana Hamaishi <luana.hamaishi@bandtec.com.br>
2023-05-25 01:43:37 -0300

    agenda

--- bodyart-android-app/BodyArtMobile/app/src/main/java/com/sptech/bodyartmobile/PerfilEstabelecimento.kt
+++ bodyart-android-app/BodyArtMobile/app/src/main/java/com/sptech/bodyartmobile/PerfilEstabelecimento.kt
@@ -1,11 +1,20 @@
 package com.sptech.bodyartmobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class PerfilEstabelecimento : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil_estabelecimento)
+
+//       Funcao voltar
+        val voltar = findViewById<TextView>(R.id.tv_back)
+        voltar.setOnClickListener{
+            val telaAnterior = Intent(applicationContext, MainActivity::class.java)
+            startActivity(telaAnterior)
+        }
     }
 }
