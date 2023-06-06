0ed4991c2765109b2c138b359e0445cf92591958
Luana Hamaishi <lhamaishi@korewireless.com>
2023-06-06 19:19:41 -0300

    algumas telas

---
+++ bodyart-android-app/BodyArtMobile/app/src/main/java/com/sptech/bodyartmobile/NovaSenha.kt
@@ -0,0 +1,11 @@
+package com.sptech.bodyartmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NovaSenha : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nova_senha)
+    }
+}
