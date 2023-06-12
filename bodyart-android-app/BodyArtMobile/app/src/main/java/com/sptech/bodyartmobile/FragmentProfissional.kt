8d128def81d7426532a262f28457b5c453bf07a3
Luana Hamaishi <lhamaishi@korewireless.com>
2023-06-12 16:37:40 -0300

    inicio fragment profissional

--- bodyart-android-app/BodyArtMobile/app/src/main/java/com/sptech/bodyartmobile/FragmentProfissional.kt
+++ bodyart-android-app/BodyArtMobile/app/src/main/java/com/sptech/bodyartmobile/FragmentProfissional.kt
@@ -8,8 +8,8 @@ import android.view.ViewGroup
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val NAME = "nome"
+private const val ID = "id"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,14 +18,13 @@ private const val ARG_PARAM2 = "param2"
  */
 class FragmentProfissional : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var nome: String? = null
+    private var id: Long = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -51,8 +50,7 @@ class FragmentProfissional : Fragment() {
         fun newInstance(param1: String, param2: String) =
             FragmentProfissional().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
                 }
             }
     }
